0aa42016dd029797c9ffef3f620864496f61bf99
dariarom94 <romanovskaya@phystech.edu>
2020-06-18 16:01:13 +0200

    initial commit of the init script

---
+++ src/00.0_init.R
@@ -0,0 +1,134 @@
+#libraries
+
+##technical
+library(simpleCache, quietly = TRUE)
+library(parallel)
+suppressMessages(library(Rsamtools))
+
+##working with data table
+suppressMessages(library(data.table))
+suppressMessages(library(dplyr))
+suppressMessages(library(tidyr))
+
+##vizualization
+suppressMessages(library(ggplot2))
+library(ggrepel)
+library(ggseqlogo)
+library(pheatmap)
+suppressMessages(library(ComplexHeatmap)) ##should be installed directly from gitHub
+suppressMessages(library(circlize))
+
+##phylogenetic trees
+suppressMessages(library(ape))
+suppressMessages(library(ggtree))
+
+
+#plotting settings
+theme_set(theme_bw())
+theme_update(axis.text = element_text(color="black"),
+             panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
+             text = element_text(size=12))
+
+pdf.options(useDingbats=FALSE)
+
+#plotting helper functions
+
+rotate_labels <- function(angle = 60,vjust = 1,hjust = 1)
+        {return(theme(axis.text.x = element_text(angle = angle, vjust = vjust,hjust = hjust)))}
+
+
+give.n <- function(x,y = NULL){
+  return(data.frame(y = ifelse(is.null(y),0,y), label = paste0("N=",length(x)))) 
+}
+
+scientific_10 <- function(x) {
+  parse(text=gsub("e", " %*% 10^", scales::scientific_format()(x)))
+}
+
+##abbreviations for plotting
+class_short <- c("Invertebrata" = "Inv.", "Chondrichthyes" = "Ch.",
+                 "Actinopteri" = "Act.", "Amphibia" = "Amp.", 
+                 "Reptilia" = "Rep.", "Aves" = "Av.", 
+                 "Marsupialia" = "Mar.", "Mammalia" = "Mam.")
+
+#colors
+class_colors <- c("Invertebrata" = "#cfcfcf",
+                  "Chondrichthyes" = "#F68383", 
+                  "Actinopteri" = "#EA4142",
+                  "Amphibia" = "#5AB349",
+                  "Reptilia" = "#8761AC",
+                  "Aves"="#FE9222",
+                  "Marsupialia"="#8EBFDA",
+                  "Mammalia"="#4892C2")
+
+tissue_colors <- c("H" = "#005fb1", "L" = "#e6ab02", "S" = "#a9a900",
+                   "M" = "#ae6600", "B" = "#D1B0FF", "G" = "#59ae00",
+                   "F" = "#00a783", "X" = "#525252")
+
+#Useful vectors
+
+#classes
+classes <- c("Invertebrata", "Chondrichthyes", "Actinopteri", "Amphibia",
+           "Reptilia", "Aves", "Marsupialia", "Mammalia")
+
+#core tissues
+core_inv <- c("Muscle","Tube_feet","Tentacle","Arm","Gills","Gonad","Pharynx")
+core_vert <- c("Liver","Heart","Brain","Spleen","Muscle","Gills","Fin")
+
+#paths (have to be updated)
+RefFreeDMA_dir = "/data/groups/lab_bock/jklughammer/gitRepos/RefFreeDMA_public/"
+convCtr_dir = "/data/groups/lab_bock/jklughammer/gitRepos/RefFreeDMA_public/auxiliary/conversionCtr/"
+BSF_dir = "/scratch/lab_bsf/samples/"
+
+data_dir = "/scratch/lab_bock/shared/projects/compEpi/"
+source_dir = file.path(Sys.getenv("CODEBASE"),"DNAmeth500species")
+rrbs_dir = "toSelf_filtered_0.08mm_final_concat"
+
+
+#paths (inherited)
+processed_dir = file.path(data_dir,"results_pipeline")
+analysis_dir = file.path(data_dir,"results_analysis")
+cache_dir = file.path(data_dir,"RCache")
+dir.create(cache_dir,showWarnings = FALSE)
+options(RCACHE.DIR = cache_dir)
+
+meta_dir = file.path(source_dir,"meta")
+scripts_dir = file.path(source_dir,"scripts")
+
+
+#annotations
+stats_annot_file <- file.path(analysis_dir,"01_basicStats/stats_annot.RData")
+
+if (file.exists(stats_annot_file)){
+  load(stats_annot_file)
+  stats_annot[,color_class:=ifelse(ncbi_class %in% c("Mammalia","Amphibia","Reptilia","Actinopteri","Chondrichthyes","Aves"),
+                                   ncbi_class,"Invertebrata"),]
+  stats_annot[ncbi_order%in%c("Diprotodontia","Dasyuromorphia"),color_class:="Marsupialia",]
+  stats_annot[,color_class:=factor(color_class,levels=classes),]
+  #remove "bad" samples
+  stats_annot=stats_annot[!grepl("Tumour|Cellline",Tissue)] 
+  }else{
+  print("stats_annot.RData doesn't exist. Run script 01.2 to create. Only loading basic sample annotation.")
+}
+
+sampleAnnot=fread(file.path(meta_dir,"Patholist_selection.tsv"))
+
+##simple helpful dataframe species-class, ordered phylogenetically 
+
+phylo_order_path <- file.path(Sys.getenv("CODEBASE"), "DNAmeth500species/meta/species_list_ordered.txt")
+if(file.exists(phylo_order_path)){
+  sp_df <- read.table(phylo_order_path)
+  colnames(sp_df) <- c("species")
+  sp_df <- unique(left_join(sp_df, stats_annot[, c("species", "color_class")]))
+  row.names(sp_df) <- sp_df$species
+}else{
+  print("species_list_ordered.txt doesn´t exist. Run 99.3 to create") ##
+}
+
+#helper functions
+my_wt=function(tab,name){
+  write.table(tab,name,sep="\t",quote=FALSE,row.names=FALSE)
+}
+
+
+
