ae565c7eed1f67f34465614d2f2a22801cd6ca82
dariarom94 <romanovskaiadaria@gmail.com>
2021-08-10 19:32:44 +0200

    redirect output

--- src/01.4_dedRef_CpG_count.R
+++ src/01.4_dedRef_CpG_count.R
@@ -2,8 +2,8 @@
 ##classical initiation
 source(file.path(Sys.getenv("CODEBASE"),"DNAmeth500species/src/00.0_init.R"))
 
-#redirecting to the stats_detail (where it is going to be analyzed)
-wd=file.path(analysis_dir,"01_basicStats/01.6_stats_detail")
+#where to save the output
+wd=file.path(analysis_dir,"01_basicStats")
 setwd(wd)
 
 #function
@@ -26,7 +26,9 @@ get_cpg_count <- function(SP){
   return(c("dedRef_count_full" = N, "dedRef_count_filtered" = N_new, 
            "CpG_count" = CpG_count, "CpG_count_filtered" = CpG_count_filtered ))
 }
+
 #lazy implementation - not the fastest, but no need to create additional files and merging them after
 ans <- sapply(sp_df$species, get_cpg_count)
+
 #saving 
 write.table(t(ans), "dedRef_CpG_count.csv", quote = F, sep = ";")
